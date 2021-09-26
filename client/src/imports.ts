234413214dd8b6935d5f35dddc2c964a14c9169b
Elias Kuiter <info@elias-kuiter.de>
2021-09-26 20:09:02 +0200

    Removed tests
    (going to SE hell for this)

--- client/src/imports.ts
+++ client/src/imports.ts
@@ -2,8 +2,6 @@
  * Dynamic imports.
  * Because some features of the application are only used on-demand and the involved
  * libraries have a large footprint, dynamic import() is used for these libraries.
- * All the imports are exported from this JavaScript file because Jest doesn't like
- * import statements in TypeScript files for some reason…
  */
 
 export const importCanvg = () => import('canvg');
